12000fbd529bb0f739d8587132e4ceda4ee79919
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-25 09:08:10 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var scene = document.getElementById('scene');
 var parallax = new Parallax(scene);
 
+
 var scene2 = document.getElementById('scene2');
-var parallax = new Parallax(scene2);
+var parallax2 = new Parallax(scene2);
 
 AOS.init();
