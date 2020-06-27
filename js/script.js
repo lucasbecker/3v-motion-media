8034588470d6b45933f2a5d43ccb7346c0dab9e5
Lucas Becker <lucasbeckerfelisberto@gmail.com>
2020-06-27 00:27:37 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -6,3 +6,20 @@ var scene2 = document.getElementById('scene2');
 var parallax2 = new Parallax(scene2);
 
 AOS.init();
+
+function iniciaPopUp(modalID){
+    const modal = document.getElementById(modalID);
+    if(modal){
+        modal.classList.add('exibe');
+        modal.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'fechar'){
+                modal.classList.remove('exibe');
+            }
+        });
+    }
+}
+
+const player = document.querySelector('#cardapioAnimado');
+player.addEventListener('click', () => {
+    iniciaPopUp('modal-exemplo');
+})
